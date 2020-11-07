036e83c82cee52c04aae20c529d670a5624e1ce4
CarlosRojasF <crojasf@gmail.com>
2020-11-08 00:08:05 +0100

    Agregado estilos Mobile y validación Js mail

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,32 @@
 // alert("hola mundo!!");
+
+
+document.getElementById("comming__button").addEventListener("click", validarEmail);
+
+	commingInput = document.getElementById("email");
+	commingErrorEmailIcon = document.getElementById("comming__error-email-icon");
+	commingErrorEmailMessage = document.getElementById("comming__error-email-message");
+
+
+
+function validarEmail() {
+
+	emailText = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+	
+	
+  if (emailText.test(commingInput.value)){
+	//  alert("La dirección de email " + campoEmail.value + " es correcta.");
+	commingInput.classList.remove('has-error');
+	commingErrorEmailIcon.classList.add('hidden');
+	commingErrorEmailMessage.classList.add('hidden');	
+
+	
+} else {
+	//  alert("La dirección de email es incorrecta.");
+	commingInput.classList.add('has-error');
+	commingErrorEmailIcon.classList.remove('hidden');
+	commingErrorEmailMessage.classList.remove('hidden');	
+  }
+}
+
+
